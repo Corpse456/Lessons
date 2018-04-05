b8450f4d9c6df5fa0c07b68f22e53810d25aeb2d
StepStudent <st@A212PC17.itstep.lan>
2018-04-05 21:38:59 +0300

    Lesson 81

---
+++ src/main/java/lesson81/coolApp/index.jsp
@@ -0,0 +1,6 @@
+<!DOCTYPE html SYSTEM "about:legacy-compat">
+<html>
+	<body>
+		<h1 style="text-align: center">Some text</h1>
+	</body>
+</html>
---
+++ src/main/java/lesson81/coolApp/servlet/MyServlet.java
@@ -0,0 +1,32 @@
+package lesson81.coolApp.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyServlet extends HttpServlet {
+    
+    int counter = 0;
+    private static final long serialVersionUID = -7187112104844868043L;
+    
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        counter++;
+        super.service(req, resp);
+        /*if ("POST".equals(req.getMethod())) {
+            doPost(req, resp);
+        }
+        if ("GET".equals(req.getMethod())) {
+            doGet(req, resp);
+        }*/
+    }    
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Inquiry accepted, GET");
+        resp.getOutputStream().println("<h1>Hello</h1>");
+    }
+}
