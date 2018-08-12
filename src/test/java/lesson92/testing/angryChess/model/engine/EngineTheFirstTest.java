db18af6d179f8d9ebf630698ec3a28c2bba35b41
Alexander Neznaev <Corpsetrup@gmail.com>
2018-08-12 12:10:19 +0200

    changed import

--- src/test/java/lesson92/testing/angryChess/model/engine/EngineTheFirstTest.java
+++ src/test/java/lesson92/testing/angryChess/model/engine/EngineTheFirstTest.java
@@ -1,7 +1,7 @@
 package lesson92.testing.angryChess.model.engine;
 
-import org.junit.jupiter.api.Test;
 import org.junit.Assert;
+import org.junit.Test;
 
 import lesson92.testing.angryChess.persistence.Player;
 import lesson92.testing.angryChess.shared_model.IGame;
@@ -12,28 +12,28 @@ class EngineTheFirstTest {
 
     @Test
     public void PlayerAreTheSame () throws ChessModelException {
-        //when 
+        // when
         long id = 1L;
         IPlayer p1 = new Player("Mocart", 1200);
         IPlayer p2 = new Player("Black Masha", 12);
-        //expect
+        // expect
         IPlayer expBlackPlayer = new Player("Black Masha", 12);
         String expWhitePlayerName = "Mocart";
-        //execute
+        // execute
         EngineTheFirst engine = new EngineTheFirst();
         IGame result = engine.createNewGame(id, p1, p2);
-        
+
         Assert.assertEquals(result.getBlackPlayer(), expBlackPlayer);
         Assert.assertEquals(result.getPlayerToMove().getName(), expWhitePlayerName);
     }
-    
+
     @Test
     public void FigureAtAStartingPosition () {
-        //то же самое по сути, только тест другой
+        // то же самое по сути, только тест другой
     }
-    
+
     @Test
     public void exceptionTest () {
-        
+
     }
 }
